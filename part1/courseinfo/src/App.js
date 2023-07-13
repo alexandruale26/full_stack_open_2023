d25b24152e50cdf0d5b16cb3be07de6e20bb6caf
Alexandru Tenie <alexandru_ale26@yahoo.com>
2023-07-13 21:48:05 +0300

    finished courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,49 @@
+const Header = (props) => <h1>{props.course.name}</h1>;
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Content = (props) => (
+  <div>
+    <Part part={props.course.parts[0]} />
+    <Part part={props.course.parts[1]} />
+    <Part part={props.course.parts[2]} />
+  </div>
+);
+
+const Total = (props) => (
+  <p>Number of exercises {props.course.parts.reduce((acc, part) => (acc += part.exercises), 0)}</p>
+);
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default App;

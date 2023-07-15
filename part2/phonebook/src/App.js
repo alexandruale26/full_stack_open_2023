2cb9265628079f079d46a6ff52b053484a2bfc2b
Alexandru Tenie <alexandru_ale26@yahoo.com>
2023-07-15 23:02:27 +0300

    finished phonebook part5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
 function App() {
-  return <div>hello</div>;
+  const data = [
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ];
+
+  const [persons, setPersons] = useState(data);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleNameChange = (e) => setNewName(e.target.value);
+  const handleNumberChange = (e) => setNewNumber(e.target.value);
+  const handleFilterChange = (e) => setFilter(e.target.value);
+
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) return alert(`${newName} is already added to phonebook!`);
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const filteredPersons = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()));
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>add a new</h3>
+
+      <PersonForm formData={{ addName: addPerson, newName, handleNameChange, newNumber, handleNumberChange }} />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={filteredPersons} />
+    </div>
+  );
 }
 
 export default App;
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,11 @@
+import Person from "./Person";
+
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map((person) => (
+      <Person key={person.name} name={person.name} number={person.number} />
+    ))}
+  </div>
+);
+
+export default Persons;

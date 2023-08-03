1e232cf90f49600555b84db0e7e5b09193602b7b
Alexandru Tenie <alexandru_ale26@yahoo.com>
2023-08-03 23:06:18 +0300

    fixes

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("body", (req, res) => {
@@ -7,6 +8,7 @@ morgan.token("body", (req, res) => {
 });
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [

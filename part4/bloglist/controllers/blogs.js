bc07548e2b5318e27c0163a8e9d6bb8f88888687
Alexandru Tenie <alexandru_ale26@yahoo.com>
2023-08-10 17:07:00 +0300

    finished bloglist step2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,16 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (_, response) => {
+  Blog.find({}).then((blogs) => response.json(blogs));
+});
+
+blogsRouter.post("/", (request, response) => {
+  const newBlog = new Blog(request.body);
+
+  newBlog.save().then((savedBlog) => {
+    response.status(201).json(savedBlog);
+  });
+});
+
+module.exports = blogsRouter;
